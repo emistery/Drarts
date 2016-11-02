df99d04be54ddef1f2f4b7e4371544744789baf5
Emiel Kok <emielkok@hotmail.com>
2016-11-02 19:59:09 +0100

    Last commit of the day

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<ul class="nav nav-tabs nav-justified  navbar-fixed-bottom">
+<ul class="nav nav-tabs nav-justified  navbar-fixed-bottom ftr-background">
   <li class="<?php echo $file == 'contact.php' ? 'active' : null ?>" role="presentation"><a href="contact.php">Contact</a></li>
   <li class="<?php echo $file == 'legal.php' ? 'active' : null ?>" role="presentation"><a href="legal.php">Legaal</a></li>
   <li class="<?php echo $file == 'about.php' ? 'active' : null ?>" role="presentation"><a href="about.php">Over ons</a></li>
