04f3a37dc501d183cda313103363bde289ab2799
Emiel Kok <emielkok@hotmail.com>
2016-11-06 14:15:02 +0100

    Small changes to navbar design

--- navbar.php
+++ navbar.php
@@ -2,8 +2,9 @@
   $parts = explode("/", $_SERVER["PHP_SELF"]);
   $file = $parts[2];
 ?>
+<div class="container-fluid">
 <nav class="navbar navbar-default">
-  <div class="container-fluid">
+  
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -32,7 +33,8 @@
       </form>
 
     </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+  
 
   
 </nav>
+</div><!-- /.container-fluid -->
