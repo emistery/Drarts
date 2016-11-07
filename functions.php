82450cf9002f7cf4b4efb2a4f073203a149e4184
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2016-11-07 15:41:27 +0100

    updated register.php

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@ function checkUsername($db, $gebruikersnaam){
 	$query = "SELECT gebruikersnaam
 			  FROM Klant
 			  WHERE gebruikersnaam = $gebruikersnaam";
-	mysqli_query($db, $query) or die('Error querying database.');
+	mysqli_query($db, $query) or die('Inlognaam bestaat al');
+
+	$result = mysqli_query($db, $query);
 	
 ?>
