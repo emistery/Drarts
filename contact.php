c7b61fbf602ce2f9c8d4f0ac7f3587c0877688a6
Pim Krips <Pim Krips>
2016-11-09 10:09:45 +0100

    Aligned 'contact.php' and changed footer.php

--- contact.php
+++ contact.php
@@ -39,31 +39,31 @@
       <!-- BODY-->
   <form class="form-horizontal" role="form" method="post" action="#form">
   <div class="form-group">
-    <label for="name" class="col-sm-2 control-label">Naam</label>
+    <label for="name" class="col-sm-1 control-label">Naam</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="name" name="name" placeholder="Voor & Achternaam" value="">
     </div>
   </div>
   <div class="form-group">
-    <label for="email" class="col-sm-2 control-label">Email</label>
+    <label for="email" class="col-sm-1 control-label">Email</label>
     <div class="col-sm-10">
       <input type="email" class="form-control" id="email" name="email" placeholder="voorbeeld@domein.nl" value="">
     </div>
   </div>
   <div class="form-group">
-    <label for="message" class="col-sm-2 control-label">Bericht</label>
+    <label for="message" class="col-sm-1 control-label">Bericht</label>
     <div class="col-sm-10">
       <textarea class="form-control" rows="4" name="message"></textarea>
     </div>
   </div>
   <div class="form-group">
-    <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+    <label for="human" class="col-sm-1 control-label">2 + 3 = ?</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="human" name="human" placeholder="Antwoord">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-10 col-sm-offset-2">
+    <div class="col-sm-10 col-sm-offset-1">
       <input id="submit" name="submit" type="submit" value="Verstuur" class="btn btn-primary">
     </div>
   </div>
--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 	<div class="row">
 		<ul class="nav nav-tabs nav-justified  navbar-fixed-bottom ftr-background">
 		<!-- Name of file is checked and marked as active page -->
-		  <li class="col-xs-4 <?php echo $file == 'contact.php' ? 'active' : null ?>" role="presentation"><a href="contact.php">Contact</a></li>
-		  <li class="col-xs-4 <?php echo $file == 'legal.php' ? 'active' : null ?>" role="presentation"><a href="legal.php">Legaal</a></li>
-		  <li class="col-xs-4 <?php echo $file == 'about.php' ? 'active' : null ?>" role="presentation"><a href="about.php">Over ons</a></li>
+		  <li class="<?php echo $file == 'contact.php' ? 'active' : null ?>" role="presentation"><a href="contact.php">Contact</a></li>
+		  <li class="<?php echo $file == 'legal.php' ? 'active' : null ?>" role="presentation"><a href="legal.php">Legaal</a></li>
+		  <li class="<?php echo $file == 'about.php' ? 'active' : null ?>" role="presentation"><a href="about.php">Over ons</a></li>
 		</ul>
 	</div>
 </div>
